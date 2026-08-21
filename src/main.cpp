e712a2bc575afb18dc59c2bb99cf00684515499d
gavdoubleu <gavin.a.woolman@durham.ac.uk>
2026-08-21 15:39:34 +0100

    Reformat to clang-format 20
    The seeding work was written against clang-format 22; CI pins 20.
    No behavioural change.

--- src/main.cpp
+++ src/main.cpp
@@ -639,8 +639,8 @@ int main(int argc, char* argv[]) {
                                                         e.getDetailMsg());
       }
     } else {
-      std::cerr << "[Rank " << rank << "] FATAL: HDF5 error: "
-                << e.getCDetailMsg() << std::endl;
+      std::cerr << "[Rank " << rank
+                << "] FATAL: HDF5 error: " << e.getCDetailMsg() << std::endl;
     }
 #ifdef USE_MPI
     MPI_Abort(MPI_COMM_WORLD, 1);
